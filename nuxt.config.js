ec1df1580534dd956e673bc96b9999b42d1ed52c
Phakphoom Khumpuan <6303013002@feu.edu>
2020-12-07 13:49:38 +0700

    Push

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: "spa",
   target: "static",
   router: {
-    base: "/boardgame/"
+    base: "/nuxt-gh-pages/",
   },
   /*
    ** Headers of the page
@@ -37,9 +37,9 @@ module.exports = {
       { property: "twitter:title", content: "Content Title" },
       {
         property: "twitter:description",
-        content: "Content description less than 200 characters"
+        content: "Content description less than 200 characters",
       },
-      { property: "twitter:image", content: "https://example.com/image.jpg" }
+      { property: "twitter:image", content: "https://example.com/image.jpg" },
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
@@ -47,71 +47,71 @@ module.exports = {
         rel: "icon",
         type: "image/png",
         sizes: "192x192",
-        href: "/android-icon-192x192.png"
+        href: "/android-icon-192x192.png",
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "96x96",
-        href: "/favicon-96x96.png"
+        href: "/favicon-96x96.png",
       },
       {
         rel: "icon",
         type: "image/png",
         sizes: "16x16",
-        href: "/favicon-16x16.png"
+        href: "/favicon-16x16.png",
       },
       {
         rel: "apple-touch-icon",
         sizes: "57x57",
-        href: "/apple-icon-57x57.png"
+        href: "/apple-icon-57x57.png",
       },
       {
         rel: "apple-touch-icon",
         sizes: "60x60",
-        href: "/apple-icon-60x60.png"
+        href: "/apple-icon-60x60.png",
       },
       {
         rel: "apple-touch-icon",
         sizes: "72x72",
-        href: "/apple-icon-72x72.png"
+        href: "/apple-icon-72x72.png",
       },
       {
         rel: "apple-touch-icon",
         sizes: "76x76",
-        href: "/apple-icon-76x76.png"
+        href: "/apple-icon-76x76.png",
       },
       {
         rel: "apple-touch-icon",
         sizes: "114x114",
-        href: "/apple-icon-114x114.png"
+        href: "/apple-icon-114x114.png",
       },
       {
         rel: "apple-touch-icon",
         sizes: "120x120",
-        href: "/apple-icon-120x120.png"
+        href: "/apple-icon-120x120.png",
       },
       {
         rel: "apple-touch-icon",
         sizes: "144x144",
-        href: "/apple-icon-144x144.png"
+        href: "/apple-icon-144x144.png",
       },
       {
         rel: "apple-touch-icon",
         sizes: "152x152",
-        href: "/apple-icon-152x152.png"
+        href: "/apple-icon-152x152.png",
       },
       {
         rel: "apple-touch-icon",
         sizes: "180x180",
-        href: "/apple-icon-180x180.png"
+        href: "/apple-icon-180x180.png",
       },
       {
         rel: "stylesheet",
         href:
-          "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
-      }
-    ]
+          "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons",
+      },
+    ],
   },
 
   /*
@@ -125,8 +125,8 @@ module.exports = {
   css: [
     {
       src: "bulma/bulma.sass",
-      lang: "sass"
-    }
+      lang: "sass",
+    },
   ],
 
   /*
@@ -139,12 +139,12 @@ module.exports = {
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    "@nuxtjs/font-awesome"
+    "@nuxtjs/font-awesome",
   ],
   /*
    ** Axios module configuration
    */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-  }
+  },
 };
